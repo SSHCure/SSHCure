c6719acd2df0ff8eff3aa1493541d02882f01489
Rick Hofstede <mail@rickhofstede.nl>
2014-10-14 16:19:05 +0200

    Added geolocation (country, city) to 'Host Details'

--- frontend/SSHCure/config.php
+++ frontend/SSHCure/config.php
@@ -3,5 +3,8 @@
     $config['backend.path']             = '/data/nfsen/plugins/SSHCure/';
     $config['database.dsn']             = 'sqlite:'.$config['backend.path'].'data/SSHCure.sqlite3';
     $config['profiling-database.dsn']   = 'sqlite:'.$config['backend.path'].'data/SSHCure_profile.sqlite3';
+
+    $config['maxmind_IPv4.path']        = 'lib/MaxMind/GeoLiteCity.dat';
+    $config['maxmind_IPv6.path']        = 'lib/MaxMind/GeoLiteCityv6.dat';
     
 ?>
--- frontend/SSHCure/json/html/get_host_details.php
+++ frontend/SSHCure/json/html/get_host_details.php
@@ -4,6 +4,7 @@
     define("TEMPLATE_PATH", "../../templates");
 
     require_once("../../config.php");
+    require_once("../../lib/MaxMind/geoipcity.inc");
     header("content-type: application/json");
 
     // Parse and process parameters
@@ -14,9 +15,34 @@
         echo json_encode($result);
         die();
     }
-    
-    $country = "The Netherlands";
-    $city = "Enschede";
+
+    if (substr_count($host, '.') == 3) {
+        $geo_db_handle = geoip_open("../../".$config['maxmind_IPv4.path'], GEOIP_STANDARD);
+    } else if (substr_count($host, ':') == 1) {
+        $geo_db_handle = geoip_open("../../".$config['maxmind_IPv6.path'], GEOIP_STANDARD);
+    } else {
+        $result['status'] = 1;
+        echo json_encode($result);
+        die();
+    }
+
+    /* Perform geolocation */
+    $geo_record = geoip_record_by_addr($geo_db_handle, $host);
+    $country = $geo_record->country_name;
+
+    if (isset($geo_record->country_name)) {
+        $country = $geo_record->country_name;
+    } else {
+        $country = "--";
+    }
+
+    if (isset($geo_record->city)) {
+        $city = $geo_record->city;
+    } else {
+        $city = "--";
+    }
+
+    geoip_close($geo_db_handle);
 
     // $db = new PDO($config['database.dsn']);
 
